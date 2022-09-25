6ee3e7eb63122dbd999cccec979893867b9a2175
Prajapati Jay <65757917+mysteriousmaster01@users.noreply.github.com>
2022-09-25 11:21:13 +0530

    Update logister.jsx

--- pages/logister.jsx
+++ pages/logister.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import logisterImg from "../public/assets/projects/002.png";
+import logisterImg from "../public/assets/projects/logister-01.png";
 import { RiRadioButtonFill } from "react-icons/ri";
 import Link from "next/link";
 
