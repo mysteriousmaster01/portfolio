78d04a1991817d1b357db922569dfeb7ea757c0c
Prajapati Jay <65757917+mysteriousmaster01@users.noreply.github.com>
2023-04-26 17:34:37 +0530

    Updated Projects

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = () => {
         <p className="text-xl tracking-widest uppercase text-[#FF8637] ">
           Projects
         </p>
-        <h2 className="py-4">Build By Me Brah!</h2>
+        <h2 className="py-4">Developed By Me</h2>
         <div className="grid md:grid-cols-2 gap-8">
 
           <Projectitem
